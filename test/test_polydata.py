00948e4796a3f8b4f12f454c16a448c8c9c8b478
Louis Pujol <louispujol@laposte.net>
2023-07-06 09:43:51 +0200

    add test for polydata/pyvista interface

--- test/test_polydata.py
+++ test/test_polydata.py
@@ -3,6 +3,61 @@ import sys
 sys.path.append(sys.path[0][:-4])
 from skshapes.data import PolyData
 import torch
+import numpy as np
+import pyvista
+
+
+def _cube():
+    points = np.array(
+        [
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 0, 1],
+            [0, 0, 1],
+            [0, 1, 0],
+            [1, 1, 0],
+            [1, 1, 1],
+            [0, 1, 1],
+        ],
+        dtype=np.float64,
+    )
+
+    faces = np.array(
+        [
+            4,
+            0,
+            1,
+            2,
+            3,
+            4,
+            0,
+            1,
+            5,
+            4,
+            4,
+            0,
+            4,
+            7,
+            3,
+            4,
+            2,
+            1,
+            5,
+            6,
+            4,
+            3,
+            2,
+            6,
+            7,
+            4,
+            4,
+            5,
+            6,
+            7,
+        ]
+    )
+
+    return pyvista.PolyData(points, faces=faces)
 
 
 def test_polydata_creation():
@@ -42,3 +97,41 @@ def test_polydata_creation():
         pass
     else:
         raise AssertionError("Assigning edges should delete triangles")
+
+
+def test_interaction_with_pyvista():
+
+    # Import/export from/to pyvista
+    import pyvista
+    from pyvista.examples import load_sphere
+
+    mesh = load_sphere()
+    n_points = mesh.n_points
+    n_triangles = mesh.n_cells
+
+    # Create a PolyData from a pyvista mesh
+    polydata = PolyData.from_pyvista(mesh)
+    assert polydata.n_points == n_points
+    assert polydata.n_triangles == n_triangles
+
+    # back to pyvista, check that the mesh is the same
+    mesh2 = polydata.to_pyvista()
+    assert np.allclose(mesh.points, mesh2.points)
+    assert np.allclose(mesh.faces, mesh2.faces)
+
+    # Open a quadratic mesh
+    cube = _cube()
+    # the cube is a polydata with 6 cells (quads) and 8 points
+    assert not cube.is_all_triangles
+    assert cube.n_cells == 6
+    assert cube.n_points == 8
+    # Create a PolyData from a pyvista mesh and check that the faces are converted to triangles
+    polydata = PolyData.from_pyvista(cube)
+    assert polydata.n_points == 8
+    assert polydata.n_triangles == 12
+    # back to pyvista, check that the mesh is the same
+    cube2 = polydata.to_pyvista()
+    assert cube2.n_cells == 12
+    assert cube2.n_points == 8
+    assert cube2.is_all_triangles
+    assert np.allclose(cube.points, cube2.points)
