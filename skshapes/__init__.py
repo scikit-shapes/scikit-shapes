8462d72e6d729124a663c238dcb28f90655c1605
Jean Feydy <jean.feydy@gmail.com>
2023-07-15 21:59:04 +0200

    Added submodules to the __init__.py.

--- skshapes/__init__.py
+++ skshapes/__init__.py
@@ -1,1 +1,9 @@
 from .types import *
+
+from .applications import *
+from .data import *
+from .loss import *
+from .morphing import *
+from .optimization import *
+from .preprocessing import *
+from .tasks import *
