fc562c4581f91378c0d0c0e2a66480f6c4ef818a
Jean Feydy <jean.feydy@gmail.com>
2023-07-15 23:32:39 +0200

    Starting to add routines to compute curvature...

---
+++ skshapes/features/curvatures.py
@@ -0,0 +1,141 @@
+import torch
+from pykeops.torch import LazyTensor
+
+# Compute tangent planes and curvatures ========================================
+
+def tangent_vectors(normals):
+    """Returns a pair of vector fields u and v to complete the orthonormal basis [n,u,v].
+
+          normals        ->             uv
+    (N, 3) or (N, S, 3)  ->  (N, 2, 3) or (N, S, 2, 3)
+
+    This routine assumes that the 3D "normal" vectors are normalized.
+    It is based on the 2017 paper from Pixar, "Building an orthonormal basis, revisited".
+
+    Args:
+        normals (Tensor): (N,3) or (N,S,3) normals `n_i`, i.e. unit-norm 3D vectors.
+
+    Returns:
+        (Tensor): (N,2,3) or (N,S,2,3) unit vectors `u_i` and `v_i` to complete
+            the tangent coordinate systems `[n_i,u_i,v_i].
+    """
+    x, y, z = normals[..., 0], normals[..., 1], normals[..., 2]
+    s = (2 * (z >= 0)) - 1.0  # = z.sign(), but =1. if z=0.
+    a = -1 / (s + z)
+    b = x * y * a
+    uv = torch.stack((1 + s * x * x * a, s * b, -s * x, b, s + y * y * a, -y), dim=-1)
+    uv = uv.view(uv.shape[:-1] + (2, 3))
+
+    return uv
+
+
+def curvatures(
+    vertices, triangles=None, scales=[1.0], batch=None, normals=None, reg=0.01
+):
+    """Returns a collection of mean (H) and Gauss (K) curvatures at different scales.
+
+    points, faces, scales  ->  (H_1, K_1, ..., H_S, K_S)
+    (N, 3), (3, N), (S,)   ->         (N, S*2)
+
+    We rely on a very simple linear regression method, for all vertices:
+
+      1. Estimate normals and surface areas.
+      2. Compute a local tangent frame.
+      3. In a pseudo-geodesic Gaussian neighborhood at scale s,
+         compute the two (2, 2) covariance matrices PPt and PQt
+         between the displacement vectors "P = x_i - x_j" and
+         the normals "Q = n_i - n_j", projected on the local tangent plane.
+      4. Up to the sign, the shape operator S at scale s is then approximated
+         as  "S = (reg**2 * I_2 + PPt)^-1 @ PQt".
+      5. The mean and Gauss curvatures are the trace and determinant of
+         this (2, 2) matrix.
+
+    As of today, this implementation does not weigh points by surface areas:
+    this could make a sizeable difference if protein surfaces were not
+    sub-sampled to ensure uniform sampling density.
+
+    For convergence analysis, see for instance
+    "Efficient curvature estimation for oriented point clouds",
+    Cao, Li, Sun, Assadi, Zhang, 2019.
+
+    Args:
+        vertices (Tensor): (N,3) coordinates of the points or mesh vertices.
+        triangles (integer Tensor, optional): (3,T) mesh connectivity. Defaults to None.
+        scales (list of floats, optional): list of (S,) smoothing scales. Defaults to [1.].
+        batch (integer Tensor, optional): batch vector, as in PyTorch_geometric. Defaults to None.
+        normals (Tensor, optional): (N,3) field of "raw" unit normals. Defaults to None.
+        reg (float, optional): small amount of Tikhonov/ridge regularization
+            in the estimation of the shape operator. Defaults to .01.
+
+    Returns:
+        (Tensor): (N, S*2) tensor of mean and Gauss curvatures computed for
+            every point at the required scales.
+    """
+    # Number of points, number of scales:
+    N, S = vertices.shape[0], len(scales)
+    ranges = diagonal_ranges(batch)
+
+    # Compute the normals at different scales + vertice areas:
+    normals_s, _ = mesh_normals_areas(
+        vertices, triangles=triangles, normals=normals, scale=scales, batch=batch
+    )  # (N, S, 3), (N,)
+
+    # Local tangent bases:
+    uv_s = tangent_vectors(normals_s)  # (N, S, 2, 3)
+
+    features = []
+
+    for s, scale in enumerate(scales):
+        # Extract the relevant descriptors at the current scale:
+        normals = normals_s[:, s, :].contiguous()  #  (N, 3)
+        uv = uv_s[:, s, :, :].contiguous()  # (N, 2, 3)
+
+        # Encode as symbolic tensors:
+        # Points:
+        x_i = LazyTensor(vertices.view(N, 1, 3))
+        x_j = LazyTensor(vertices.view(1, N, 3))
+        # Normals:
+        n_i = LazyTensor(normals.view(N, 1, 3))
+        n_j = LazyTensor(normals.view(1, N, 3))
+        # Tangent bases:
+        uv_i = LazyTensor(uv.view(N, 1, 6))
+
+        # Pseudo-geodesic squared distance:
+        d2_ij = ((x_j - x_i) ** 2).sum(-1) * ((2 - (n_i | n_j)) ** 2)  # (N, N, 1)
+        # Gaussian window:
+        window_ij = (-d2_ij / (2 * (scale ** 2))).exp()  # (N, N, 1)
+
+        # Project on the tangent plane:
+        P_ij = uv_i.matvecmult(x_j - x_i)  # (N, N, 2)
+        Q_ij = uv_i.matvecmult(n_j - n_i)  # (N, N, 2)
+        # Concatenate:
+        PQ_ij = P_ij.concat(Q_ij)  # (N, N, 2+2)
+
+        # Covariances, with a scale-dependent weight:
+        PPt_PQt_ij = P_ij.tensorprod(PQ_ij)  # (N, N, 2*(2+2))
+        PPt_PQt_ij = window_ij * PPt_PQt_ij  #  (N, N, 2*(2+2))
+
+        # Reduction - with batch support:
+        PPt_PQt_ij.ranges = ranges
+        PPt_PQt = PPt_PQt_ij.sum(1)  # (N, 2*(2+2))
+
+        # Reshape to get the two covariance matrices:
+        PPt_PQt = PPt_PQt.view(N, 2, 2, 2)
+        PPt, PQt = PPt_PQt[:, :, 0, :], PPt_PQt[:, :, 1, :]  # (N, 2, 2), (N, 2, 2)
+
+        # Add a small ridge regression:
+        PPt[:, 0, 0] += reg
+        PPt[:, 1, 1] += reg
+
+        # (minus) Shape operator, i.e. the differential of the Gauss map:
+        # = (PPt^-1 @ PQt) : simple estimation through linear regression
+        S = torch.solve(PQt, PPt).solution
+        a, b, c, d = S[:, 0, 0], S[:, 0, 1], S[:, 1, 0], S[:, 1, 1]  # (N,)
+
+        # Normalization
+        mean_curvature = a + d
+        gauss_curvature = a * d - b * c
+        features += [mean_curvature.clamp(-1, 1), gauss_curvature.clamp(-1, 1)]
+
+    features = torch.stack(features, dim=-1)
+    return features
---
+++ skshapes/features/normals.py
@@ -0,0 +1,102 @@
+import torch
+from pykeops.torch import LazyTensor
+from ..utils import diagonal_ranges
+
+from ..types import (
+    typecheck,
+    Points,
+    Optional,
+    Triangles
+)
+
+@typecheck
+def mesh_normals_areas(*,
+                       vertices: Points, 
+                       triangles=Optional[Triangles], 
+                       scale=[1.0], 
+                       batch=None, 
+                       normals=Optional[Points]):
+    """Returns a smooth field of normals, possibly at different scales.
+
+    points, triangles or normals, scale(s)  ->      normals
+    (N, 3),    (3, T) or (N,3),      (S,)   ->  (N, 3) or (N, S, 3)
+
+    Simply put - if `triangles` are provided:
+      1. Normals are first computed for every triangle using simple 3D geometry
+         and are weighted according to surface area.
+      2. The normal at any given vertex is then computed as the weighted average
+         of the normals of all triangles in a neighborhood specified
+         by Gaussian windows whose radii are given in the list of "scales".
+
+    If `normals` are provided instead, we simply smooth the discrete vector
+    field using Gaussian windows whose radii are given in the list of "scales".
+
+    If more than one scale is provided, normal fields are computed in parallel
+    and returned in a single 3D tensor.
+
+    Args:
+        vertices (Tensor): (N,3) coordinates of mesh vertices or 3D points.
+        triangles (integer Tensor, optional): (3,T) mesh connectivity. Defaults to None.
+        scale (list of floats, optional): (S,) radii of the Gaussian smoothing windows. Defaults to [1.].
+        batch (integer Tensor, optional): batch vector, as in PyTorch_geometric. Defaults to None.
+        normals (Tensor, optional): (N,3) raw normals vectors on the vertices. Defaults to None.
+
+    Returns:
+        (Tensor): (N,3) or (N,S,3) point normals.
+        (Tensor): (N,) point areas, if triangles were provided.
+    """
+
+    # Single- or Multi-scale mode:
+    if hasattr(scale, "__len__"):
+        scales, single_scale = scale, False
+    else:
+        scales, single_scale = [scale], True
+    scales = torch.Tensor(scales).type_as(vertices)  # (S,)
+
+    # Compute the "raw" field of normals:
+    if triangles is not None:
+        # Vertices of all triangles in the mesh:
+        A = vertices[triangles[0, :]]  # (N, 3)
+        B = vertices[triangles[1, :]]  # (N, 3)
+        C = vertices[triangles[2, :]]  # (N, 3)
+
+        # Triangle centers and normals (length = surface area):
+        centers = (A + B + C) / 3  # (N, 3)
+        V = (B - A).cross(C - A)  # (N, 3)
+
+        # Vertice areas:
+        S = (V ** 2).sum(-1).sqrt() / 6  # (N,) 1/3 of a triangle area
+        areas = torch.zeros(len(vertices)).type_as(vertices)  # (N,)
+        areas.scatter_add_(0, triangles[0, :], S)  # Aggregate from "A's"
+        areas.scatter_add_(0, triangles[1, :], S)  # Aggregate from "B's"
+        areas.scatter_add_(0, triangles[2, :], S)  # Aggregate from "C's"
+
+    else:  # Use "normals" instead
+        areas = None
+        V = normals
+        centers = vertices
+
+    # Normal of a vertex = average of all normals in a ball of size "scale":
+    x_i = LazyTensor(vertices[:, None, :])  # (N, 1, 3)
+    y_j = LazyTensor(centers[None, :, :])  # (1, M, 3)
+    v_j = LazyTensor(V[None, :, :])  # (1, M, 3)
+    s = LazyTensor(scales[None, None, :])  # (1, 1, S)
+
+    D_ij = ((x_i - y_j) ** 2).sum(-1)  #  (N, M, 1)
+    K_ij = (-D_ij / (2 * s ** 2)).exp()  # (N, M, S)
+
+    # Support for heterogeneous batch processing:
+    if batch is not None:
+        batch_vertices = batch
+        batch_centers = batch[triangles[0, :]] if triangles is not None else batch
+        K_ij.ranges = diagonal_ranges(batch_vertices, batch_centers)
+
+    if single_scale:
+        U = (K_ij * v_j).sum(dim=1)  # (N, 3)
+    else:
+        U = (K_ij.tensorprod(v_j)).sum(dim=1)  # (N, S*3)
+        U = U.view(-1, len(scales), 3)  # (N, S, 3)
+
+    normals = F.normalize(U, p=2, dim=-1)  # (N, 3) or (N, S, 3)
+
+    return normals, areas
