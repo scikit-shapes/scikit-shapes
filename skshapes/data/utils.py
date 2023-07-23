06512b9477c153fcec34e3325035d61f861525d2
Jean Feydy <jean.feydy@gmail.com>
2023-07-23 12:25:28 +0200

    Exploring caching...

--- skshapes/data/utils.py
+++ skshapes/data/utils.py
@@ -211,3 +211,94 @@ class DataAttributes(dict):
         raise ValueError(
             "You cannot change the device of the set after the creation of the DataAttributes object, use .to(device) to make a copy of the DataAttributes object on the new device"
         )
+
+
+if False:
+    class cached_property(object):
+        """
+        A property that is only computed once per instance and then replaces itself
+        with an ordinary attribute. Deleting the attribute resets the property."""
+
+        def __init__(self, func):
+            self.__doc__ = getattr(func, '__doc__')
+            self.func = func
+
+        def __get__(self, obj, cls):
+            if obj is None:
+                return self
+            value = obj.__dict__[self.func.__name__] = self.func(obj)
+            return value
+
+else:
+    from functools import cached_property
+
+
+import functools
+import weakref
+
+def cached_method(*lru_args, **lru_kwargs):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(self, *args, **kwargs):
+            # We're storing the wrapped method inside the instance. If we had
+            # a strong reference to self the instance would never die.
+            self_weak = weakref.ref(self)
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+        return wrapped_func
+    return decorator
+
+
+from functools import cached_property, lru_cache, partial, update_wrapper
+from typing import Callable, Optional, TypeVar, Union
+
+T = TypeVar("T") 
+
+def instance_lru_cache(
+    method: Optional[Callable[..., T]] = None,
+    *,
+    maxsize: Optional[int] = 128,
+    typed: bool = False
+) -> Union[Callable[..., T], Callable[[Callable[..., T]], Callable[..., T]]]:
+    """Least-recently-used cache decorator for instance methods.
+
+    The cache follows the lifetime of an object (it is stored on the object,
+    not on the class) and can be used on unhashable objects. Wrapper around
+    functools.lru_cache.
+
+    If *maxsize* is set to None, the LRU features are disabled and the cache
+    can grow without bound.
+
+    If *typed* is True, arguments of different types will be cached separately.
+    For example, f(3.0) and f(3) will be treated as distinct calls with
+    distinct results.
+
+    Arguments to the cached method (other than 'self') must be hashable.
+
+    View the cache statistics named tuple (hits, misses, maxsize, currsize)
+    with f.cache_info().  Clear the cache and statistics with f.cache_clear().
+    Access the underlying function with f.__wrapped__.
+
+    """
+
+    def decorator(wrapped: Callable[..., T]) -> Callable[..., T]:
+        def wrapper(self: object) -> Callable[..., T]:
+            return lru_cache(maxsize=maxsize, typed=typed)(
+                update_wrapper(partial(wrapped, self), wrapped)
+            )
+
+        return cached_property(wrapper)  # type: ignore
+
+    return decorator if method is None else decorator(method)
+
+
+def reload(self):
+    """Reload all cached properties."""
+    cls = self.__class__
+    attrs = [a for a in dir(self) if isinstance(getattr(cls, a, cls), cached_property)]
+    for a in attrs:
+        delattr(self, a)
