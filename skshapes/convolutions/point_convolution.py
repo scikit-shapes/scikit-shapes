9fff3ce4cba1c4e385a7368760483d1c93525db0
Louis-Pujol <louispujol@laposte.net>
2023-09-28 16:32:57 +0200

    forgot to stage point_convolution.py

--- skshapes/convolutions/point_convolution.py
+++ skshapes/convolutions/point_convolution.py
@@ -10,8 +10,8 @@ class LinearOperator:
     """A simple wrapper for scaled linear operators."""
 
     def __init__(self, matrix, input_scaling=None, output_scaling=None):
-        N, M = matrix.shape
-        assert matrix.shape == (N, M)
+        M, N = matrix.shape
+        assert matrix.shape == (M, N)
         assert input_scaling is None or input_scaling.shape == (N,)
         assert output_scaling is None or output_scaling.shape == (M,)
 
@@ -56,26 +56,37 @@ def _point_convolution(
     geodesic: bool = False,
     normalize: bool = False,
     dtype: Optional[Literal["float", "double"]] = None,
+    target=None,
 ) -> LinearOperator:
-    """Creates a convolution kernel on a PolyData as a (N, N) linear operator."""
+    """Creates a convolution kernel on a PolyData as a (N, N) linear operator if no target is provided,
+    or as a (M, N) linear operator if a target is provided"""
+
+    if target is None:
+        target = self
+
     N = self.n_points
+    M = target.n_points
+
     weights_j = self.point_weights
     assert weights_j.shape == (N,)
 
     X = self.points
+    Y = target.points
 
     if dtype == "float":
         X = X.float()
+        Y = Y.float()
         weights_j = weights_j.float()
     elif dtype == "double":
         X = X.double()
+        Y = Y.double()
         weights_j = weights_j.double()
 
     backend_args = dict(window=window, cutoff=cutoff, geodesic=geodesic)
 
     if scale is None:
         # scale = +infinity, the kernel is always equal to 1
-        K_ij = constant_1_kernel(points=X, **backend_args)
+        K_ij = constant_1_kernel(points=X, target_points=Y, **backend_args)
 
     else:
         if kernel == "gaussian":
@@ -83,6 +94,7 @@ def _point_convolution(
             # ahead of time instead of scaling the distances for every pair of points.
             sqrt_2 = 1.41421356237
             X = X / (sqrt_2 * scale)
+            Y = Y / (sqrt_2 * scale)
 
             # For dense computations, users may specify a cutoff as a kernel
             # value below which the kernel is assumed to be zero.
@@ -93,12 +105,14 @@ def _point_convolution(
 
             K_ij = squared_distances(
                 points=X,
+                target_points=Y,
                 kernel=lambda d2: (-d2).exp(),
                 **backend_args,
             )
 
         elif kernel == "uniform":
             X = X / scale
+            Y = Y / scale
             # For dense computations, users may specify a cutoff as a kernel
             # value below which the kernel is assumed to be zero.
             # For the uniform kernel, this just means discarding pairs of points
@@ -108,16 +122,115 @@ def _point_convolution(
 
             K_ij = squared_distances(
                 points=X,
+                target_points=Y,
                 kernel=lambda d2: 1.0 * (d2 <= 1),
                 **backend_args,
             )
 
     if normalize:
+        print(K_ij.shape)
+        print(weights_j.shape)
+
         total_weights_i = K_ij @ weights_j
-        assert total_weights_i.shape == (N,)
+        assert total_weights_i.shape == (M,)
         norm_i = 1.0 / total_weights_i
 
     else:
         norm_i = None
+
+    assert K_ij.shape == (M, N)
+
+    return LinearOperator(K_ij, input_scaling=weights_j, output_scaling=norm_i)
+
+
+def convolution(
+    *,
+    source,
+    target,
+    kernel: Literal["uniform", "gaussian"] = "gaussian",
+    scale: Optional[Number] = None,
+    window: Literal[None, "ball", "knn", "spectral"] = None,
+    cutoff: Optional[Number] = None,
+    geodesic: bool = False,
+    normalize: bool = False,
+    dtype: Optional[Literal["float", "double"]] = None,
+):
+    """Same as PolyData.point_convolution, but with source and target as arguments."""
+
+    N = source.n_points
+    M = target.n_points
+
+    weights_j = source.point_weights
+    assert weights_j.shape == (N,)
+
+    X = source.points
+    Y = target.points
+
+    if dtype == "float":
+        X = X.float()
+        Y = Y.float()
+        weights_j = weights_j.float()
+    elif dtype == "double":
+        X = X.double()
+        Y = Y.double()
+        weights_j = weights_j.double()
+
+    backend_args = dict(window=window, cutoff=cutoff, geodesic=geodesic)
+
+    if scale is None:
+        # scale = +infinity, the kernel is always equal to 1
+        K_ij = constant_1_kernel(points=X, target_points=Y, **backend_args)
+
+    else:
+        if kernel == "gaussian":
+            # Divisions are expensive: whenever possible, it's best to scale the points
+            # ahead of time instead of scaling the distances for every pair of points.
+            sqrt_2 = 1.41421356237
+            X = X / (sqrt_2 * scale)
+            Y = Y / (sqrt_2 * scale)
+
+            # For dense computations, users may specify a cutoff as a kernel
+            # value below which the kernel is assumed to be zero.
+            # exp(-x^2) <= cutoff <=> x^2 >= -log(cutoff)
+            if window is None and cutoff is not None and cutoff < 1:
+                assert cutoff > 0
+                backend_args["cutoff"] = -np.log(cutoff)
+
+            K_ij = squared_distances(
+                points=X,
+                target_points=Y,
+                kernel=lambda d2: (-d2).exp(),
+                **backend_args,
+            )
+
+        elif kernel == "uniform":
+            X = X / scale
+            Y = Y / scale
+            # For dense computations, users may specify a cutoff as a kernel
+            # value below which the kernel is assumed to be zero.
+            # For the uniform kernel, this just means discarding pairs of points
+            # which are at distance > 1 (after rescaling).
+            if window is None and cutoff is not None:
+                backend_args["cutoff"] = 1.01  # To be on the safe side...
+
+            K_ij = squared_distances(
+                points=X,
+                target_points=Y,
+                kernel=lambda d2: 1.0 * (d2 <= 1),
+                **backend_args,
+            )
+
+    if normalize:
+        print(K_ij.shape)
+        print(weights_j.shape)
+
+        total_weights_i = K_ij @ weights_j
+        assert total_weights_i.shape == (M,)
+        norm_i = 1.0 / total_weights_i
+
+    else:
+        norm_i = None
+
+    assert K_ij.shape == (M, N)
 
     return LinearOperator(K_ij, input_scaling=weights_j, output_scaling=norm_i)
