29d6e6f11719bcd2c9cf737ddee7cf743bafa773
Louis-Pujol <louispujol@laposte.net>
2023-10-31 16:15:37 +0100

    Check annotations of classes instances not classes
    in the check_annotations tests for losses and morphings, we checked
    methods had the right annotations at the class level, without
    instantiate. Then for L2 loss, which is a wrapper of Lp loss, there
    was an error as __init__ and __call__ are not defined in the definition
    of L2loss even if actual l2loss objects has these attributes

--- test/test_modules_annotations.py
+++ test/test_modules_annotations.py
@@ -3,6 +3,23 @@ from typing import Union, get_origin, get_args
 import skshapes as sks
 
 
+def check_annotations(annotations, reference_annotations):
+    # Loop over the annotations
+    for key in reference_annotations.keys():
+        assert key in annotations.keys()
+        if get_origin(reference_annotations[key]) is Union:
+            # If the reference annotation is a Union, check that the
+            # annotation is the Union itself or one of its arguments
+            # useful for example as Shape is defined as Union of specific
+            # shapes structures (PolyData, Image, etc.) and
+            # some losses/morphings are limited to specific shapes
+            assert annotations[key] == reference_annotations[
+                key
+            ] or annotations[key] in get_args(reference_annotations[key])
+        else:
+            assert issubclass(annotations[key], reference_annotations[key])
+
+
 def check_module_annotations(module, template, type):
     """Check that the type and the classes' annotations of the methods of a
     module are correct. More precisely, we check that all the classes in the
@@ -17,47 +34,37 @@ def check_module_annotations(module, template, type):
         type : the desired type of the classes in the module
     """
 
-    def check_annotations(annotations, reference_annotations):
-        # Loop over the annotations
-        for key in reference_annotations.keys():
-            assert key in annotations.keys()
-            if get_origin(reference_annotations[key]) is Union:
-                # If the reference annotation is a Union, check that the
-                # annotation is the Union itself or one of its arguments
-                # useful for example as Shape is defined as Union of specific
-                # shapes structures (PolyData, Image, etc.) and
-                # some losses/morphings are limited to specific shapes
-                assert annotations[key] == reference_annotations[
-                    key
-                ] or annotations[key] in get_args(reference_annotations[key])
-            else:
-                assert issubclass(annotations[key], reference_annotations[key])
-
     # Find the classes in the module
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
+    for name, cl in inspect.getmembers(module):
+        if inspect.isclass(cl):
             # Check that the class is a subclass of the given type
-            assert issubclass(obj, type)
+            assert issubclass(cl, type)
 
-            # Check that the methods of the template are in the class
-            for method_name in template.keys():
-                assert method_name in obj.__dict__.keys()
+            try:
+                # instantiate the class
+                obj = cl()
+            except TypeError:
+                raise AssertionError(
+                    f"Class {cl} cannot be instantiated with no arguments"
+                )
 
-            # Loop over the methods of the class
             print("Inspecting annotations of class {}:".format(name))
-            for method_name, method in inspect.getmembers(obj):
-                if method_name in template.keys():
-                    print("Method name: {}".format(method_name))
-                    # get the annotations of the method
-                    annotations = method.__annotations__
-                    print("Annotations: {}".format(annotations))
-                    print(
-                        "Reference annotations: {}".format(
-                            template[method_name]
-                        )
+            for method_name in template.keys():
+                print("Method name: {}".format(method_name))
+                # Check that the method is in the class
+                assert hasattr(obj, method_name)
+                annotations = getattr(obj, method_name).__annotations__
+                print("Annotations: {}".format(annotations))
+                print(
+                    "Reference annotations: {}".format(
+                        template[method_name]
                     )
-                    # Check that the annotations are correct
-                    check_annotations(annotations, template[method_name])
+                )
+                # Check that the annotations are correct
+                check_annotations(
+                    annotations=annotations,
+                    reference_annotations=template[method_name],
+                )
 
 
 # Define the templates for the annotations of Loss and Morphing
