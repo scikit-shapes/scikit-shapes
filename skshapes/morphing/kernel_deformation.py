1603c6f7a42d83cdaf013eace973cc8330a77af8
Louis-Pujol <louispujol@laposte.net>
2023-11-01 10:33:27 +0100

    doctring kernel_deformation

--- skshapes/morphing/kernel_deformation.py
+++ skshapes/morphing/kernel_deformation.py
@@ -1,3 +1,13 @@
+"""This module implements the kernel deformation morphing algorithm.
+
+This algorithm decribes the morphing as a deformation of the ambiant space.
+The parameter is a vector field, which is referred to as the momentum. This
+momentum is smoothed by a kernel, and the morphed shape is obtained by
+integrating the momentum. The regularization is given by <p, K_q p> where
+p is the momentum, q is the initial position of the shape and K_q is the kernel
+at q.
+"""
+
 from __future__ import annotations
 
 import torch
@@ -12,6 +22,8 @@ from .kernels import Kernel, GaussianKernel
 
 
 class KernelDeformation(BaseModel):
+    """Kernel deformation morphing algorithm"""
+
     @typecheck
     def __init__(
         self,
@@ -20,6 +32,16 @@ class KernelDeformation(BaseModel):
         kernel: Kernel = GaussianKernel(),
         **kwargs,
     ) -> None:
+        """Initialize the model
+
+        Args:
+            n_steps (int, optional): Number of integration steps.
+                Defaults to 1.
+            integrator (Integrator, optional): Hamiltonian integrator.
+                Defaults to EulerIntegrator().
+            kernel (Kernel, optional): Kernel used to smooth the momentum.
+                Defaults to GaussianKernel().
+        """
         self.integrator = integrator
         self.cometric = kernel
         self.n_steps = n_steps
@@ -32,6 +54,20 @@ class KernelDeformation(BaseModel):
         return_path: bool = False,
         return_regularization: bool = False,
     ) -> MorphingOutput:
+        """Morph a shape using the kernel deformation algorithm
+
+        Args:
+            shape (polydata_type): the shape to morph
+            parameter (Points): the momentum
+            return_path (bool, optional): True if you want to have access to
+                the morphing's sequence of polydatas. Defaults to False.
+            return_regularization (bool, optional): True to have access to the
+                regularization. Defaults to False.
+
+        Returns:
+            MorphingOutput: a named tuple containing the morphed shape, the
+                regularization and the path if needed.
+        """
         if parameter.device != shape.device:
             p = parameter.to(shape.device)
         else:
@@ -73,4 +109,12 @@ class KernelDeformation(BaseModel):
 
     @typecheck
     def parameter_shape(self, shape: polydata_type) -> tuple[int, int]:
+        """Return the shape of the parameter
+
+        Args:
+            shape (polydata_type): the shape to morph
+
+        Returns:
+            tuple[int, int]: the shape of the parameter
+        """
         return shape.points.shape
