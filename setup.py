8a16b24c0bbf2b5aba6a4b133fae5084b1d5096e
Louis-Pujol <louispujol@laposte.net>
2024-01-23 16:10:32 +0100

    [skip ci] clean setup.py

--- setup.py
+++ setup.py
@@ -11,43 +11,6 @@ edges_extraction = Extension(
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
 )
 
-
-# dependencies = [
-#     "numpy",
-#     "torch>=1.11",
-#     "torchdiffeq",
-#     "pykeops",
-#     "geomloss",
-#     "jaxtyping",
-#     "beartype>=0.16.1",
-#     "pyvista",
-#     "vedo",
-#     "fast-simplification",
-# ]
-
-# submodules = [
-#     "applications",
-#     "convolutions",
-#     "multiscaling",
-#     "data",
-#     "triangle_mesh",
-#     "decimation",
-#     "features",
-#     "loss",
-#     "morphing",
-#     "optimization",
-#     "tasks",
-# ]
-
 setup(
-    # name="scikit-shapes",
-    # version="1.0",
-    # description="Shape Analysis in Python",
-    # author="Scikit-Shapes Developers",
-    # author_email="skshapes@gmail.com",
-    # url="",
-    # install_requires=dependencies,
-    # packages=["skshapes"]
-    # + ["skshapes." + submodule for submodule in submodules],
     ext_modules=cythonize([edges_extraction]),
 )
