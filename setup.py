98399b0dd321c401685b12d40768dc09a9d8e8da
Louis-Pujol <louispujol@laposte.net>
2024-01-26 17:06:13 +0100

    [skip ci] move skshapes to src/skshapes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import Extension, setup
 edges_extraction = Extension(
     name="skshapes.triangle_mesh.edges_extraction",
     sources=[
-        "skshapes/triangle_mesh/edges_extraction.pyx",
+        "src/skshapes/triangle_mesh/edges_extraction.pyx",
     ],
     include_dirs=[np.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
