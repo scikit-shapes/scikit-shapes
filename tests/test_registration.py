1e582e948ac9081c3fb01aa258f5d88df226f864
Louis-Pujol <louispujol@laposte.net>
2024-02-01 14:49:57 +0100

    speedup lddm_control_points

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -188,24 +188,24 @@ def test_registration_device():
 
 def test_lddmm_control_points():
     """Test to run LDDMM with control points."""
-    mesh1 = sks.PolyData(pyvista.Sphere()).decimate(target_reduction=0.95)
-    mesh2 = sks.PolyData(pyvista.Sphere()).decimate(target_reduction=0.9)
+    mesh1 = sks.PolyData(pyvista.Sphere()).decimate(target_reduction=0.99)
+    mesh2 = sks.PolyData(pyvista.Sphere()).decimate(target_reduction=0.98)
 
-    mesh1.control_points = mesh1.bounding_grid(N=5, offset=0.25)
+    mesh1.control_points = mesh1.bounding_grid(N=3, offset=0.25)
 
     # Define the model
     model = sks.ExtrinsicDeformation(
-        n_steps=5, kernel=sks.GaussianKernel(sigma=0.5), control_points=True
+        n_steps=2, kernel=sks.GaussianKernel(sigma=0.5), control_points=True
     )
 
     loss = sks.OptimalTransportLoss()
-    optimizer = sks.LBFGS()
+    optimizer = sks.SGD()
 
     registration = sks.Registration(
         model=model,
         loss=loss,
         optimizer=optimizer,
-        n_iter=10,
+        n_iter=2,
         regularization=0.1,
         gpu=False,
         verbose=True,
