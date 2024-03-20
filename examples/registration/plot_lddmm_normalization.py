c2ace4a5d567822d02a0095df8fdb1f90d70a1e6
Louis-Pujol <louispujol@laposte.net>
2024-03-20 08:52:38 +0100

    update normalization example

--- examples/registration/plot_lddmm_normalization.py
+++ examples/registration/plot_lddmm_normalization.py
@@ -1,28 +1,34 @@
 """
-LDDMM with normalized cometric
-==============================
-
+LDDMM with normalized kernel
+============================
 This notebook illustrates the interest of normalizing the cometric in the LDDMM
 model. We consider the registration of two spheres that differ by a translation.
 
-Without normalization of the cometric, the carpooling artifact occurs: the sphere
-is contracted, then translated and finally expanded.
+Without normalization, the carpooling artifact occurs: the sphere
+is contracted, then translated and finally expanded. in this situation, event if
+the morphed shape matches the target, the intermediate shapes are not meaningful
+and the extrapolation is not reliable.
 
-Adding normalization (algorithm 5.7 in https://www.jeanfeydy.com/geometric_data_analysis.pdf)
-leads to guaranteed preservation of translations of geodesics in shape space.
+Normalizing the kernel adds regularization to the morphing, leanding to prevention
+of the carpooling artifact and improvement of the extrapolation to some extent.
 
-See p177 of https://www.jeanfeydy.com/geometric_data_analysis.pdf for further details.
+Options for normalization are:
+
+* "rows": normalize the rows of the kernel
+* "columns": normalize the columns of the kernel
+* "both": normalize both the rows and the columns of the kernel (for quare kernels, algorithm 5.7 in https://www.jeanfeydy.com/geometric_data_analysis.pdf)
+
+Further explanation can be found in the p177 and onwards of https://www.jeanfeydy.com/geometric_data_analysis.pdf.
+
 """
 
-# %% [markdown]
-# Load data
-#
-
-# %%
 import pyvista as pv
 import torch
 
 import skshapes as sks
+
+###############################################################################
+# Load data
 
 plot_kwargs = {
     "smooth_shading": True,
@@ -47,13 +53,11 @@ plotter.add_mesh(target.to_pyvista(), color="red", opacity=0.8, **plot_kwargs)
 plotter.camera_position = cpos
 plotter.show()
 
-# %% [markdown]
-#LDDM without normalization
-#--------------------------
-#
-#
 
-# %%
+###############################################################################
+# LDDM without normalization
+# --------------------------
+
 model = sks.ExtrinsicDeformation(
     n_steps=4,
     kernel="gaussian",
@@ -82,10 +86,10 @@ for i in range(len(path)):
 plotter.camera_position = cpos
 plotter.show()
 
-# %% [markdown]
+
+###############################################################################
 # Extrapolation
 
-# %%
 back = model.morph(
     shape=source,
     parameter=task.parameter_,
@@ -114,13 +118,72 @@ for i in range(len(path)):
     plotter.write_frame()
 plotter.close()
 
-# %% [markdown]
-#LDDM with normalization
-#-----------------------
-#
-# Explanation of the model:
+###############################################################################
+# Normalizing the rows of the kernel
+# ----------------------------------
 
-# %%
+model_norm = sks.ExtrinsicDeformation(
+    n_steps=4,
+    kernel="gaussian",
+    scale=0.3,
+    normalization="rows",
+)
+
+task_norm = sks.Registration(
+    model=model_norm,
+    loss=loss,
+    optimizer=sks.LBFGS(),
+    n_iter=3,
+    regularization_weight=0.0,
+    verbose=True,
+)
+
+task_norm.fit(source=source, target=target)
+path = task_norm.path_
+
+plotter = pv.Plotter()
+plotter.add_mesh(source.to_pyvista(), color="teal", opacity=0.2, **plot_kwargs)
+plotter.add_mesh(target.to_pyvista(), color="red", opacity=0.2, **plot_kwargs)
+for i in range(len(path)):
+    plotter.add_mesh(path[i].to_pyvista(), color="tan", opacity=0.8, **plot_kwargs)
+plotter.camera_position = cpos
+plotter.show()
+
+###############################################################################
+# Extrapolation
+
+back = model_norm.morph(
+    shape=source,
+    parameter=task_norm.parameter_,
+    final_time=-1.0,
+    return_path=True,
+).path
+
+model_norm.n_steps = 8
+forward = model_norm.morph(
+    shape=source,
+    parameter=task_norm.parameter_,
+    final_time=2.0,
+    return_path=True,
+).path
+
+path = back[::-1] + forward[1:]
+
+plotter = pv.Plotter()
+plotter.open_gif("lddmm_normalization.gif", fps=4)
+for i in range(len(path)):
+    plotter.clear_actors()
+    plotter.add_mesh(source.to_pyvista(), color="teal", opacity=0.2, **plot_kwargs)
+    plotter.add_mesh(target.to_pyvista(), color="red", opacity=0.2, **plot_kwargs)
+    plotter.add_mesh(path[i].to_pyvista(), color="tan", opacity=0.8, **plot_kwargs)
+    plotter.camera_position = cpos
+    plotter.write_frame()
+plotter.close()
+
+###############################################################################
+# Normalizing both rows and columns of the kernel
+# -----------------------------------------------
+
 model_norm = sks.ExtrinsicDeformation(
     n_steps=4,
     kernel="gaussian",
@@ -148,11 +211,9 @@ for i in range(len(path)):
 plotter.camera_position = cpos
 plotter.show()
 
-
-# %% [markdown]
+###############################################################################
 # Extrapolation
 
-# %%
 back = model_norm.morph(
     shape=source,
     parameter=task_norm.parameter_,
