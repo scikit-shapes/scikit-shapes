e3ef4fcf80eb4abd8d28a52618a5433de60e7bb3
Jean Feydy <jean.feydy@gmail.com>
2024-11-28 16:31:45 +0100

    Moved knn_graph and k_ring_graph to skshapes.topology.

---
+++ examples/features/README.rst
@@ -0,0 +1,6 @@
+.. _features_examples:
+
+Shape features
+----------------
+
+Scikit-shapes provides allows you to compute a variety of shape features, including point normals, curvatures and shape descriptors.
--- src/skshapes/data/polydata.py
+++ src/skshapes/data/polydata.py
@@ -413,6 +413,8 @@ class PolyData(polydata_type):
 
     # N.B.: _cached_methods is also used in the decorator add_cached_methods_to_sphinx.
     _cached_methods = (
+        "knn_graph",
+        "k_ring_graph",
         "mesh_convolution",
         "point_convolution",
         "point_curvature_colors",
@@ -438,6 +440,7 @@ class PolyData(polydata_type):
         _point_quadratic_fits,
         _point_shape_indices,
     )
+    from ..topology import _k_ring_graph, _knn_graph
     from .utils import cache_clear
 
     # ----------------------------------------------------------------------------
@@ -895,125 +898,6 @@ class PolyData(polydata_type):
                 )
 
             self._stiff_edges = stiff_edges.clone().to(self.device)
-
-    @typecheck
-    def k_ring_graph(self, k: int = 2, verbose: bool = False) -> Edges:
-        """Compute the k-ring graph of the shape.
-
-        The k-ring graph is the graph where each vertex is connected to its
-        k-neighbors, where the neighborhoods are defined by the edges.
-
-        Parameters
-        ----------
-        k, optional
-            the size of the neighborhood, by default 2
-        verbose, optional
-            whether or not display information about the remaining number of steps
-
-        Returns
-        -------
-            The k-ring neighbors edges.
-        """
-        if not k > 0:
-            msg = "The number of neighbors k must be positive"
-            raise ValueError(msg)
-
-        edges = self.edges
-
-        if verbose:
-            print(f"Computing k-neighbors graph with k = {k}")  # noqa: T201
-            print(f"Step 1/{k}")  # noqa: T201
-
-        # Compute the adjacency matrix
-        adjacency_matrix = torch.zeros(
-            size=(self.n_points, self.n_points),
-            dtype=int_dtype,
-            device=self.device,
-        )
-        for i in range(self.n_edges):
-            i0, i1 = self.edges[i]
-            adjacency_matrix[i0, i1] = 1
-            adjacency_matrix[i1, i0] = 1
-
-        M = adjacency_matrix
-        k_ring_edges = edges.clone()
-        k_ring_edges = torch.sort(k_ring_edges, dim=1).values
-
-        for i in range(k - 1):
-
-            if verbose:
-                print(f"Step {i+2}/{k}")  # noqa: T201
-
-            # Iterate the adjacency matrix
-            M = M @ adjacency_matrix
-
-            # Get the connectivity (at least one path between the two vertices)
-            a, b = torch.where(M > 0)
-
-            last_edges = torch.stack((a, b), dim=1)
-
-            # Remove the diagonal and the duplicates with the convention
-            # edges[i, 0] < edges[i, 1]
-            last_edges = last_edges[
-                torch.where(last_edges[:, 0] - last_edges[:, 1] < 0)
-            ]
-
-            # Concatenate the new edges and remove the duplicates
-            k_ring_edges = torch.cat((k_ring_edges, last_edges), dim=0)
-            k_ring_edges = torch.unique(k_ring_edges, dim=0)
-
-        return k_ring_edges
-
-    @typecheck
-    def knn_graph(self, k: int = 2, include_edges: bool = False) -> Edges:
-        """Return the k-nearest neighbors edges of a shape.
-
-        Parameters
-        ----------
-        shape
-            The shape to process.
-        k
-            The number of neighbors.
-        include_edges
-            If True, the edges of the shape are concatenated with the k-nearest
-            neighbors edges.
-
-        Returns
-        -------
-        Edges
-            The k-nearest neighbors edges.
-        """
-        if not k > 0:
-            msg = "The number of neighbors k must be positive"
-            raise ValueError(msg)
-
-        from pykeops.torch import LazyTensor
-
-        points = self.points
-
-        points_i = LazyTensor(points[:, None, :])  # (N, 1, 3)
-        points_j = LazyTensor(points[None, :, :])  # (1, N, 3)
-
-        D_ij = ((points_i - points_j) ** 2).sum(-1)
-
-        out = D_ij.argKmin(K=k + 1, dim=1)
-
-        out = out[:, 1:]
-
-        knn_edges = torch.zeros(
-            (k * self.n_points, 2), dtype=int_dtype, device=self.device
-        )
-        knn_edges[:, 0] = torch.repeat_interleave(
-            torch.arange(self.n_points), k
-        )
-        knn_edges[:, 1] = out.flatten()
-
-        if include_edges and self.edges is not None:
-            knn_edges = torch.cat([self.edges, knn_edges], dim=0)
-
-        knn_edges, _ = torch.sort(knn_edges, dim=1)
-
-        return torch.unique(knn_edges, dim=0)
 
     #################################
     #### Triangles getter/setter ####
