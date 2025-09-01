a1a41f2e5fa36fae06bb578c85ce676bc18e13b2
guillaumeHoury <guillaume.houry@live.fr>
2025-09-01 16:29:52 +0200

    Defined the Image class and basic methods

--- src/skshapes/__init__.py
+++ src/skshapes/__init__.py
@@ -1,6 +1,6 @@
 """Scikit-Shapes: shape analysis in python."""
 
-from ._data import Circle, PolyData, Sphere
+from ._data import Circle, PolyData, Sphere, Image
 from .applications import *
 from .convolutions import *
 from .doc import *
@@ -23,6 +23,7 @@ __version__ = "0.3.1"
 
 __all__ = [
     "PolyData",
+    "Image",
     "Sphere",
     "Circle",
     "types",
